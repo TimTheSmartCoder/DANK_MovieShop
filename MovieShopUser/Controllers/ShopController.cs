7f1105fa983e2174b49250b7db49cf19914ea9c6
mortenjull <mortenjull@hotmail.com>
2016-10-05 13:04:11 +0200

    Made MovieShopUser solution working on controler and the main movieshop view

---
+++ MovieShopUser/Controllers/ShopController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieShopBackend;
+using MovieShopBackend.Entities;
+using MovieShopUser.Models;
+
+namespace MovieShopUser.Controllers
+{
+    public class ShopController : Controller
+    {
+        private IManager<Movie> _movieManager = new ManagerFacade().GetMovieManager();
+        private IManager<Genre> _GenreManager = new ManagerFacade().GetGenreManager();
+
+        private List<Movie> movies = new List<Movie>();
+        private List<Genre> genres = new List<Genre>();
+
+        public ShopController()
+        {
+            movies = _movieManager.ReadAll();
+            genres = _GenreManager.ReadAll();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            GenreMovieViewModel viewModel = new GenreMovieViewModel
+            {
+                Genres = genres,
+                Movies = movies
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ MovieShopUser/Models/GenreMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieShopBackend.Entities;
+
+namespace MovieShopUser.Models
+{
+    public class GenreMovieViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public List<Genre> Genres { get; set; }
+    }
+}
