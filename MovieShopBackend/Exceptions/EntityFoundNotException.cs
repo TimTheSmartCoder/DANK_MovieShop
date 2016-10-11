6be9c9b43835b034657c114e867ac0e1b21feaf1
Tim Knudsen <TimTheSmartCoder@outlook.dk>
2016-10-11 19:19:25 +0200

    Added AbstractManager and Exception, plus updated other managers to use AbstractManager.

---
+++ MovieShopBackend/Exceptions/EntityFoundNotException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieShopBackend.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        /// <summary>
+        /// MovieShopBackendException exception for handling exception in the
+        /// backend.
+        /// </summary>
+        /// <param name="message">Erorr message.</param>
+        public EntityNotFoundException(string message) : base(message)
+        {
+            //No code is needed for the constructor.
+        }
+    }
+}
