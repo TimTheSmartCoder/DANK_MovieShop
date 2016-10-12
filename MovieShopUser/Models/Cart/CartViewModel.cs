a810cd991a5e2d0e2294c3b02aeefce0185dddea
stormman744 <mtv6310@gmail.com>
2016-10-12 12:14:18 +0200

    added cartmanager, add method in controller (this redirects to page where add is called from)

---
+++ MovieShopUser/Models/Cart/CartViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieShopBackend.Entities;
+
+namespace MovieShopUser.Models.Cart
+{
+    public class CartViewModel
+    {
+        private List<Movie> _movies;
+
+        public List<Movie> Movies
+        {
+            get { return _movies; }
+            set
+            {
+                if (value != null)
+                {
+                    _movies = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Movies can not be null");
+                }
+            }
+        }
+    }
+}
