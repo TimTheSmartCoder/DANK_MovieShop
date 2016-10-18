c01c5582ab0bb97e178335d15153bf00616cea94
Tim Knudsen <TimTheSmartCoder@outlook.dk>
2016-10-18 23:17:25 +0200

    Added Checkout functionality, plus installed AutoMapper in MovieShopUser.

---
+++ MovieShopUser/Controllers/CheckoutController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MovieShopBackend;
+using MovieShopBackend.Entities;
+using MovieShopUser.Models.Checkout;
+
+namespace MovieShopUser.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private IManager<Customer> _customerManager = new ManagerFacade().GetCustomerManager();
+        private IManager<Address> _addressManager = new ManagerFacade().GetAddressManager();
+        private IManager<Order> _OrderManager = new ManagerFacade().GetOrderManager();
+
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            return RedirectToAction("Email");
+        }
+
+        [HttpGet]
+        public ActionResult Email()
+        {
+            return View(new CheckoutEmailViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Email([Bind(Include = "Email")] CheckoutEmailViewModel checkoutEmailViewModel)
+        {
+            if (checkoutEmailViewModel == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            if (ModelState.IsValid)
+            {
+                //Be aware off that we are only getting all of the customers here, because we do not want to
+                //mess up are interfaces, normally you would get the id of the logged in user and use the id of
+                //the user to get the customer we are currently viewing.
+                Customer customer =
+                    this._customerManager.ReadAll()
+                        .FirstOrDefault(c => c.Email == checkoutEmailViewModel.Email);
+
+                return RedirectToAction("Process", new { email = checkoutEmailViewModel.Email});
+            }
+
+            return View(checkoutEmailViewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Process(string email)
+        {
+            //Be aware off that we are only getting all of the customers here, because we do not want to
+            //mess up are interfaces, normally you would get the id of the logged in user and use the id of
+            //the user to get the customer we are currently viewing.
+            Customer customer =
+                this._customerManager.ReadAll()
+                    .FirstOrDefault(c => c.Email == email);
+
+            CheckoutProcessViewModel checkoutProcessViewModel;
+
+            if (customer != null)
+            {
+                checkoutProcessViewModel = AutoMapper.Mapper.Map<CheckoutProcessViewModel>(customer);
+                AutoMapper.Mapper.Map(customer.Address, checkoutProcessViewModel);
+            }
+            else
+                checkoutProcessViewModel = new CheckoutProcessViewModel();
+
+            return View(checkoutProcessViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Process([Bind(Include = "Id,FirstName,LastName,Email,StreetName,StreetNumber,Country,ZipCode")] CheckoutProcessViewModel checkoutProcessViewModel)
+        {
+            if (checkoutProcessViewModel == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            if (ModelState.IsValid)
+            {
+                Customer customer = AutoMapper.Mapper.Map<Customer>(checkoutProcessViewModel);
+                customer.Address = AutoMapper.Mapper.Map<Address>(checkoutProcessViewModel);
+
+                if (checkoutProcessViewModel.Id == -1)
+                {
+                    Customer c = this._customerManager.Create(customer);
+
+                    //TEMPORARY -------
+                    List<Movie> movies = new List<Movie>();
+                    foreach (var movie in new ManagerFacade().GetMovieManager().ReadAll())
+                    {
+                        movie.Orders = null;
+                        movies.Add(movie);
+                    }
+                    //TEMPORARY -------
+
+                    Order order = new Order()
+                    {
+                        CustomerId = c.Id,
+                        Date = DateTime.Now,
+                        Movies = movies
+                    };
+
+                    this._OrderManager.Create(order);
+                }
+                else
+                {
+                    Customer c = this._customerManager.Update(customer);
+                    c.Address = this._addressManager.Update(customer.Address);
+                    
+                    //TEMPORARY -------
+                    List<Movie> movies = new List<Movie>();
+                    foreach (var movie in new ManagerFacade().GetMovieManager().ReadAll())
+                    {
+                        movie.Orders = null;
+                        movies.Add(movie);
+                    }
+                    //TEMPORARY -------
+
+                    Order order = new Order()
+                    {
+                        CustomerId = c.Id,
+                        Date = DateTime.Now,
+                        Movies = movies
+                    };
+
+                    this._OrderManager.Create(order);
+                }
+                
+                return RedirectToAction("Success");
+            }
+
+            return View(checkoutProcessViewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieShopUser/Models/Checkout/CheckoutEmailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieShopUser.Models.Checkout
+{
+    public class CheckoutEmailViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
